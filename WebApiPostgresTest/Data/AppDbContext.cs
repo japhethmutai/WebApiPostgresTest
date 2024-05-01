987ccbd0248d06bee7331a9da5b56235f86c07fc
japheth <japhemchirie@gmail.com>
2024-05-01 12:31:59 +0300

    Add project files.

---
+++ WebApiPostgresTest/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiPostgresTest.Data
+{
+    public class AppDbContext: DbContext
+    {
+        protected readonly IConfiguration _configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Connect to postgres with connection string from app settings
+            options.UseNpgsql(_configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
